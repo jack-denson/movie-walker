ca7335f85cd5a62ba656ddf137ed5e659c1828c7
Jack Denson <jackdenson47@gmail.com>
2022-12-27 17:23:58 -0500

    Added TODO for final path validation

---
+++ api/valid_path.js
@@ -0,0 +1,3 @@
+// TODO: routes that consume an entire path, that validate whether the path is connected or not
+
+// Semi-optional if this ends up just being a front-end game, not so much if we add scores & accounts and stuff
