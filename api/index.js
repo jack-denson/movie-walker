06253b00effdb6d162128decc55b2c05b21be350
Jack Denson <jackdenson47@gmail.com>
2022-12-27 17:23:41 -0500

    Add links routes to enable gameplay

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv  = require('dotenv');
+const links   = require('./links');
+
 const port = 4000;
 
 async function run() {
@@ -7,6 +9,8 @@ async function run() {
 
     app.use( express.json() );
     app.use( express.urlencoded({ extended: true }) );
+
+    await links.route( app, [] );
 
     app.listen( port, () => {
         console.log(`Listening on port ${ port }`);
---
+++ api/links.js
@@ -0,0 +1,101 @@
+const axios = require('axios');
+
+const crewToInclude = [ 'Director', 'Executive Producer', 'Producer' ];
+
+async function getLinkData( req, res ) {
+    const { is_film } = req.query || true;
+    const { to } = req.params;
+
+    // TODO: Check cache first(redis?)
+
+    if( is_film ) {
+        const film_data = await getFilmData( to )
+        return res.json( film_data )
+    } else {
+        const person_data = await getPersonData( to )
+
+        return res.json( person_data )
+    }
+
+}
+
+async function getFilmData( film_id ) {
+    const tmdb_url = `https://api.themoviedb.org/3/movie/${ film_id }?api_key=${ process.env.TMDB_API_KEY }&append_to_response=credits`
+
+    const tmdb_response = await axios.get( tmdb_url );
+    const film_data = tmdb_response.data;
+    const { poster_path, original_title, release_date, credits, id } = film_data;
+
+    credits.cast = credits.cast.map( castMember => {
+        return {
+            name: castMember.name,
+            role: castMember.character,
+            profile_path: castMember.profile_path,
+            tmdb_id: castMember.id
+        }
+    });
+    credits.crew = credits.crew.filter(
+        crewMember => crewToInclude.includes( crewMember.job )
+    ).map( crewMember => {
+        return {
+            name: crewMember.name,
+            tmdb_id: crewMember.id,
+            profile_path: crewMember.profile_path,
+
+        }
+    });
+
+    return {
+        poster_path,
+        title: original_title,
+        release: release_date.substr( 0, 4 ),
+        tmdb_id: id,
+        credits,
+        is_film: true
+    }
+}
+
+async function getPersonData( person_id ) {
+    const tmdb_url = `https://api.themoviedb.org/3/person/${ person_id }?api_key=${ process.env.TMDB_API_KEY }&append_to_response=movie_credits`;
+
+    const tmdb_response = await axios.get( tmdb_url );
+    const person_data = tmdb_response.data;
+    const { name, id, movie_credits, profile_path } = person_data;
+
+    movie_credits.cast = movie_credits.cast.map( movie => {
+        return {
+            role: movie.character,
+            tmdb_id: movie.id,
+            title: movie.title,
+            poster_path: movie.poster_path,
+            release: movie.release_date.substr( 0, 4 )
+        }
+    });
+    movie_credits.crew = movie_credits.crew.filter(
+        movie => crewToInclude.includes( movie.job )
+    ).map( movie => {
+        return {
+            job: movie.job,
+            tmdb_id: movie.id,
+            title: movie.title,
+            poster_path: movie.poster_path,
+            release: movie.release_date.substr( 0, 4 )
+        }
+    });
+
+    return {
+        name,
+        tmdb_id: id,
+        movie_credits,
+        profile_path
+    }
+
+
+
+}
+
+module.exports = {
+    route: async ( app, middleware ) => {
+        app.get( '/link/:to', ...middleware, getLinkData );
+    }
+}
