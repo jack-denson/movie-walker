5d713f866789431521902e211b8e30ef360187a8
Jack Denson <jackdenson47@gmail.com>
2022-12-27 23:57:58 -0500

    Added is_film property to challenge endpoint

--- api/challenges.js
+++ api/challenges.js
@@ -7,12 +7,14 @@ async function getChallenge( req, res ) {
             "poster_path": "/8j58iEBw9pOXFD2L0nt0ZXeHviB.jpg",
             "title": "Once Upon a Time… in Hollywood",
             "release": "2019",
-            "tmdb_id": 466272
+            "tmdb_id": 466272,
+            "is_film": true
         },
         {
             "name": "Frances McDormand",
             "tmdb_id": 3910,
-            "profile_path": "/r0A7hZsM1zuavTr0jN7bwmBcliR.jpg"
+            "profile_path": "/r0A7hZsM1zuavTr0jN7bwmBcliR.jpg",
+            "is_film": false
         }
     ]
 
