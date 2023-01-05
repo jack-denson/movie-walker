8c40d56b649214b02d7c8fa6ab08aac735771512
Jack Denson <jackdenson47@gmail.com>
2023-01-05 18:01:01 -0500

    Dynamic challenge from backend

--- public/src/Game.js
+++ public/src/Game.js
@@ -1,67 +1,35 @@
 import React from 'react'
+import axios from 'axios';
 import './Game.css'
 import Path from './Path.js';
 import ChallengeIndicator from './ChallengeIndicator';
 import MovieNodeView from './MovieNodeView';
 import WinScreen from './WinScreen';
-import { Divider } from '@mui/material';
+import { CircularProgress, Divider } from '@mui/material';
+
 
 class Game extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      challenge: [
-        {
-            "poster_path": "/xi8Iu6qyTfyZVDVy60raIOYJJmk.jpg",
-            "title": "Reservoir Dogs",
-            "release": "1992",
-            "tmdb_id": 500,
-            "is_film": true
-        },
-        {
-            "name": "Frances McDormand",
-            "tmdb_id": 3910,
-            "profile_path": "/r0A7hZsM1zuavTr0jN7bwmBcliR.jpg",
-            "is_film": false
-        }
-      ],
-      foundPath: [
-        {
-            "poster_path": "/xi8Iu6qyTfyZVDVy60raIOYJJmk.jpg",
-            "title": "Reservoir Dogs",
-            "release": "1992",
-            "tmdb_id": 500,
-            "is_film": true
-        },
-        {
-            "name": "Quentin Tarantino",
-            "tmdb_id": 138,
-            "profile_path": "/1gjcpAa99FAOWGnrUvHEXXsRs7o.jpg",
-            "is_film": false
-        },
-        {
-            "poster_path": "/8j58iEBw9pOXFD2L0nt0ZXeHviB.jpg",
-            "title": "Inglourious Basterds",
-            "release": "2009",
-            "tmdb_id": 16869,
-            "is_film": true
-        },
-        {
-            "name": "Brad Pitt",
-            "tmdb_id": 287,
-            "profile_path": "/oTB9vGIBacH5aQNS0pUM74QSWuf.jpg",
-            "is_film": false
-        },
-        // {
-        //     "poster_path": "/jdwSkQu3XirmX18MNj8CqFWsCk.jpg",
-        //     "title": "Burn After Reading",
-        //     "release": "2008",
-        //     "tmdb_id": 4944,
-        //     "is_film": true
-        // },
-      ]
+      challenge: [],
+      foundPath: []
     };
   }
+
+  async componentDidMount() {
+    this.fetchChallenge();
+  }
+
+  async fetchChallenge() {
+    const api_url = 'http://localhost:4000/challenge/';
+    const { data: api_res } = await axios.get( api_url );
+    this.setState({
+      challenge: api_res,
+      foundPath: [ api_res[ 0 ] ]
+    });
+  }
+
 
   takeLink( nextNode ) {
     const {
@@ -120,7 +88,8 @@ class Game extends React.Component {
   }
 
   render() {
-    return (
+    if( this.state.challenge.length ) {
+      return (
         <div>
             <ChallengeIndicator challenge={ this.state.challenge }></ChallengeIndicator>
             <Divider style={{ background: 'white' }} />
@@ -137,7 +106,12 @@ class Game extends React.Component {
               path={this.state.foundPath}
               gameWon={ this.state.foundPath[ this.state.foundPath.length - 1].tmdb_id == this.state.challenge[ 1 ].tmdb_id }></WinScreen>
         </div>
-    );
+      );
+    } else {
+      return <div className="loadingGame">
+        <CircularProgress />
+      </div>
+    }
 
   }
 }
